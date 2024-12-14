1f6a14c55bf1a1091331271997786c9532499906
TBSten <81161390+TBSten@users.noreply.github.com>
2024-12-14 11:03:52 +0900

    build-logic を追加

---
+++ build-logic/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+}
+
+// TODO
+group = "com.example.mylibrary.buildLogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinGradlePlugin)
+    implementation(libs.kotlinGradlePluginApi)
+    implementation(libs.ktlintGradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("lint") {
+            id = "$group.lint"
+            implementationClass = "LintConventionPlugin"
+        }
+    }
+}
